217c07402904fbe0b4bd57c7246e41c320d6d2f1
benjamin427 <shadowboxer79@hotmail.com>
2026-05-11 14:58:34 -0400

    Included an event error handler

--- test/home/end_to_end_test/verify_flight_purchase_1.spec.js
+++ test/home/end_to_end_test/verify_flight_purchase_1.spec.js
@@ -6,6 +6,9 @@ test.beforeEach(async({page}) => {
     const verifyFlightServices = new blazedemo_home(page)
     const referenceTitle = process.env.BLAZEDEMO_HOME_TITLE
     await verifyFlightServices.gotoWebsite()
+    await page.on('pageerror', (error) => {
+        console.error('Page error: ', error)
+    })
     await expect(page).toHaveTitle(referenceTitle) 
 })
 test("Verify the total cost of a flight from Paris to Buenos Aires from Virgin America airlines", async({page}) => {
